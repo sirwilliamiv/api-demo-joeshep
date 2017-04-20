5f01c5a0b09c85ab6bb117152ca478b46d11acb4
sirwilliamiv <wjconnolly2@gmail.com>
2017-04-20 11:01:49 -0500

    working on updating tables for heroku

--- db/migrations/20170417110724_shows.js
+++ db/migrations/20170417110724_shows.js
@@ -12,24 +12,24 @@ exports.up = function(knex, Promise) {
     table.timestamp('datAdded').notNullable().defaultTo(knex.fn.now());
     table.integer('show_id').unsigned().references('shows.id');
   })
-  .createTable('directors', (table) => {
-    table.increments();
-    table.string('name').notNullable().unique();
-    table.string('gender').notNullable();
-    table.integer('birthYear');
-    table.string('twitterHandle');
-  })
-  .createTable('shows_directors', (table) => {
-    table.increments();
-    table.integer('director_id').unsigned().references('directors.id');
-    table.integer('show_id').unsigned().references('shows.id');
-  })
+  // .createTable('directors', (table) => {
+  //   table.increments();
+  //   table.string('name').notNullable().unique();
+  //   table.string('gender').notNullable();
+  //   table.integer('birthYear');
+  //   table.string('twitterHandle');
+  // })
+  // .createTable('shows_directors', (table) => {
+  //   table.increments();
+  //   table.integer('director_id').unsigned().references('directors.id');
+  //   table.integer('show_id').unsigned().references('shows.id');
+  // })
 };
 
 exports.down = (knex, Promise) => {
   return knex.schema
-  .dropTable('shows_directors')
+  // .dropTable('shows_directors')
   .dropTable('favorites')
-  .dropTable('directors')
+  // .dropTable('directors')
   .dropTable('shows');
 };
